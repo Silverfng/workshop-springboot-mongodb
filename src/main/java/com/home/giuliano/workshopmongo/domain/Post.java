2c6df0e19f3eb3958c808fc2055c4dd949982f9f
Ilson Giuliano <ilsongiuliano@yahoo.com.br>
2024-01-30 10:48:14 -0300

    Acrescentando comentarios aos posts

--- src/main/java/com/home/giuliano/workshopmongo/domain/Post.java
+++ src/main/java/com/home/giuliano/workshopmongo/domain/Post.java
@@ -1,11 +1,14 @@
 package com.home.giuliano.workshopmongo.domain;
 
 import com.home.giuliano.workshopmongo.dto.AuthorDTO;
+import com.home.giuliano.workshopmongo.dto.CommentDTO;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 @Document
@@ -17,6 +20,10 @@ public class Post implements Serializable {
     private String title;
     private String body;
     private AuthorDTO author;
+
+
+    private List<CommentDTO> comments = new ArrayList<>();
+
 
     public Post() {
     }
@@ -69,6 +76,14 @@ public class Post implements Serializable {
         this.author = author;
     }
 
+    public List<CommentDTO> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<CommentDTO> comments) {
+        this.comments = comments;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -81,6 +96,7 @@ public class Post implements Serializable {
     public int hashCode() {
         return Objects.hash(id);
     }
+
 
 
 }
