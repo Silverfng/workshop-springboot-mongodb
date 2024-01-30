96416d615580c2241b64baf6509626ed24bfe874
Ilson Giuliano <ilsongiuliano@yahoo.com.br>
2024-01-30 10:35:34 -0300

    Endpoint para retornar os posts de um usuario

--- src/main/java/com/home/giuliano/workshopmongo/resources/UserResource.java
+++ src/main/java/com/home/giuliano/workshopmongo/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.home.giuliano.workshopmongo.resources;
 
+import com.home.giuliano.workshopmongo.domain.Post;
 import com.home.giuliano.workshopmongo.domain.User;
 import com.home.giuliano.workshopmongo.dto.UserDTO;
 import com.home.giuliano.workshopmongo.services.UserService;
@@ -53,6 +54,12 @@ public class UserResource {
         obj.setId(id);
         obj = service.update(obj);
         return ResponseEntity.noContent().build();
+    }
+
+    @RequestMapping(value="/{id}/posts", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj.getPosts());
     }
 
 }
