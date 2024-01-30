28235510fcaa18b298b44f3475600fd8930f353e
Ilson Giuliano <ilsongiuliano@yahoo.com.br>
2024-01-29 23:23:18 -0300

    Criando entity POST com User aninhado

---
+++ src/main/java/com/home/giuliano/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package com.home.giuliano.workshopmongo.repository;
+
+import com.home.giuliano.workshopmongo.domain.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+}
