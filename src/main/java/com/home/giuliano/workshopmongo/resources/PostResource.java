a4959b6f6b9c4d46dabe592fb6577b7fdcbf0f79
Ilson Giuliano <ilsongiuliano@yahoo.com.br>
2024-01-30 11:03:04 -0300

    Consulta simples com query methods

--- src/main/java/com/home/giuliano/workshopmongo/resources/PostResource.java
+++ src/main/java/com/home/giuliano/workshopmongo/resources/PostResource.java
@@ -3,6 +3,7 @@ package com.home.giuliano.workshopmongo.resources;
 import com.home.giuliano.workshopmongo.domain.Post;
 import com.home.giuliano.workshopmongo.domain.User;
 import com.home.giuliano.workshopmongo.dto.UserDTO;
+import com.home.giuliano.workshopmongo.resources.util.URL;
 import com.home.giuliano.workshopmongo.services.PostService;
 import com.home.giuliano.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,6 +27,13 @@ public class PostResource {
     public ResponseEntity<Post> findById(@PathVariable String id) {
         Post obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @RequestMapping(value="/titlesearch", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+        text = URL.decodeParam(text);
+        List<Post> list = service.findByTitle(text);
+        return ResponseEntity.ok().body(list);
     }
 
 }
